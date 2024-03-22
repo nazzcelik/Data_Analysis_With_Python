2a51b3ac90a6db1f6a2deef6a486f12471e63026
Naz ÇELİK <nazclk1903@gmail.com>
2024-03-22 03:44:42 +0300

    Data Analysis With Pandas

---
+++ data_analysis_with_Pandas.py
@@ -0,0 +1,327 @@
+#####################
+# Pandas Series
+#####################
+import pandas as pd
+
+s = pd.Series([10, 77, 12, 4, 5])
+type(s)
+s.index
+s.dtype
+s.size
+s.ndim
+s.values
+type(s.values)
+s.head(3)
+s.tail(3)
+
+###########################
+# Veri Okuma (Reading Data)
+###########################
+import pandas as pd
+
+df = pd.read_excel("test.xlsx")
+df.head(4)
+
+#    Unnamed: 0     TV  RADİO  NEWSPAPER  SALES
+# 0           1  230.0   37.8       69.2   22.1
+# 1           2   44.5   39.3       45.1   10.4
+# 2           3   17.2   45.9       69.0    9.3
+# 3           4  151.6   41.0       58.5   18.5
+
+
+########################################
+# Veriye Hızlı Bakış (Quick Look at Data)
+########################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+df["alive"].value_counts()
+
+################################################
+# Pandas'ta Seçim İşlemleri (Selection in pandas)
+################################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.index
+df[0:10]
+df.drop(0, axis=0).head()
+
+delete_indexes = [1, 3, 5, 7]
+df.drop(delete_indexes, axis=0).head()
+
+#############################
+# Değişkeni Index'e Çevirmek
+#############################
+
+df["age"].head()
+
+df.index = df["age"]
+
+df.drop("age", axis=1).head()
+
+df.drop("age", axis=1, inplace=True)
+df.head()
+
+#############################
+# Index'i Değişkene Çevirmek
+#############################
+df.index
+
+df["age"] = df.index
+df.head()
+# ikinci yol
+
+df.reset_index().head()
+df = df.reset_index()
+df.head()
+
+##################################
+# Değişkenler Üzerinde İşlemler
+##################################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+"age" in df
+
+df["age"].head()
+df.age.head()
+type(df["age"].head())
+
+df[["age"]].head()
+type(df[["age"]].head())
+
+df[["age", "alive"]]
+
+col_names = ["age", "adult_male", "alive"]
+df[col_names]
+
+df["age2"] = df["age"] ** 2
+df["age3"] = df["age"] / df["age2"]
+
+df.drop("age3", axis=1).head()
+df.drop("age3", axis=1).head()
+
+df.drop(col_names, axis=1).head()
+
+df.loc[:, ~df.columns.str.contains("age")].head()
+
+####################
+# iloc & loc
+####################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+# iloc: integer based selection
+df.iloc[0:3]
+df.iloc[0, 0]
+# loc: label based selection
+df.loc[0:3]
+
+df.iloc[0:3, 0:3]
+df.loc[0:3, "age"]
+
+col_names = ["age", "embareked", "alive"]
+df.loc[0:3, col_names]
+
+########################################
+# Koşullu Seçim (Conditional Selection)
+########################################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[df["age"] > 50].head()
+df[df["age"] > 50]["age"].count()
+
+df.loc[df["age"] > 50, ["age", "class"]].head()
+
+df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
+
+df["embark_town"].value_counts()
+
+df_new = df.loc[(df["age"] > 50)
+                & (df["sex"] == "male")
+                & ((df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton")),
+["age", "class", "embark_town"]].head()
+df_new["embark_town"].value_counts()
+
+####################################################
+# Toplulaitırma ve Gruplama (Aggregation & Grouping)
+####################################################
+
+# - count()
+# - first()
+# - last()
+# - mean()
+# - median()
+# - min()
+# - max()
+# - std()
+# - var()
+# - sum()
+# - pivot table
+
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age"].mean()
+
+df.groupby("sex")["age"].mean()
+
+df.groupby("sex").agg({"age": "mean"})
+
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"],
+                       "survived": "mean"})
+
+df.groupby(["sex", "embark_town"]).agg({"age": ["mean"],
+                                        "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": ["mean"],
+                                                 "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({
+    "age": ["mean"],
+    "survived": "mean",
+    "sex": "count"})
+
+######################
+# Pivot table
+######################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df.pivot_table("survived", "sex", "embarked")
+
+df.pivot_table("survived", "sex", ["embarked", "class"])
+
+df.head()
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
+
+df.pivot_table("survived", "sex", ["new_age", "class"])
+
+pd.set_option("display.width", 500)
+
+######################
+# Apply ve Lambda
+######################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"] * 2
+df["age3"] = df["age"] * 5
+
+# 1.yol
+(df["age"] / 10).head()
+(df["age2"] / 10).head()
+(df["age3"] / 10).head()
+
+for col in df.columns:
+    if "age" in col:
+        print(col)
+
+for col in df.columns:
+    if "age" in col:
+        print((df[col] / 10).head())
+
+for col in df.columns:
+    if "age" in col:
+        df[col] == df[col] / 10
+
+df.head()
+
+# 2.yol
+df[["age", "age2", "age3"]].apply(lambda x: x / 10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x / 10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+# df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.head()
+
+##############################
+# BİRLEŞTİRME (JOİN) İŞLEMLERİ
+##############################
+
+# Concat ile birleştirme işlemleri
+##################################
+import numpy as np
+import pandas as pd
+
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+df2 = df1 + 99
+
+pd.concat([df1, df2])
+
+pd.concat([df1, df2], ignore_index=True)
+
+# Merge ile birleştirme işlemleri
+##################################
+
+df1 = pd.DataFrame({"employees": ["john", "dennis", "mark", "maria"],
+                    "group": ["accounting", "engineering", "hr"]})
+
+df2 = pd.DataFrame({"employees": ["mark", "john", "dennis", "maria"],
+                    "start_date": [2010, 2009, 2014, 2019]})
+
+pd.merge(df1, df2)
+pd.merge(df1, df2, on="employees")
+
+# Amaç: Her çalışanın müdürünün bilgisine erişmek istiyoruz.
+df3 = pd.merge(df1, df2)
+
+df4 = pd.DataFrame({"group": ["accounting", "engineering", "hr"],
+                    "manager": ["Ali", "Veli", "Meli"]})
+
+pd.merge(df3, df4)
